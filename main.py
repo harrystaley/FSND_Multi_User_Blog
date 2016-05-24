bbf3cfcc4f944459c4c16fd0ca56b9c21963626a
Harry Staley <staleyh@gmail.com>
2016-05-24 00:20:13 -0500

    FEAT: implemented ninja templates and modularized code
    I implemented Jinja templates in my application and modularized my code
    by separating the code into different file types such as html and
    python. I also included my first push up to googles cloud platform.

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
+import os
+import jinja2
 import webapp2
 
-form = """
-<form method="post" action="/testform">
-    <input name="q">
-    <input type="submit" name="submit">
-</form>
-"""
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.environment(loader = jinja2.FileSystemLoader(template_dir))
 
 
-class MainPage(webapp2.RequestHandler):
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+class MainPage(Handler):
     def get(self):
-        # self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(form)
+        self.render("shopping_list.html")
 
+        # output = form_html
+        # output_hidden = ""
 
-class TestHandler(webapp2.RequestHandler):
-    def post(self):
-        q = self.request.get("q")
-        self.response.out.write(q)
-
-        # self.response.headers['Content-Type'] = 'text/plain'
-        # self.response.write(self.request)
-
+        # items = self.request.get_all("food")
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
