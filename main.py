c6d81414dba3d3869ceea51d161aadd6189a28ac
Harry Staley <staleyh@gmail.com>
2016-05-26 20:48:45 -0500

    FEAT: Added auto escaping of markup
    Auto escaping of markup enabled in main to escape HTML or other code
    that is input into the text box.

--- main.py
+++ main.py
@@ -5,36 +5,36 @@ import webapp2
 # sets the locaiton of the templates folder contained in the home of this file.
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 # envokes the jinja2 envronment pointing it to the location of the templates.
-JINJA_ENV = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR))
+# with markup automatically escaped
+JINJA_ENV = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR),
+                               autoescape=True)
 
 
 class Handler(webapp2.RequestHandler):
     """
-    Handler class for all functions in this app for rendering any templates.
+    Handler class for all functions in this app for rendering
+    any templates.
     """
     def write(self, *a, **kw):
-        """
-        displays the respective function, parameters, ect.
-        """
+        """ displays the respective function, parameters, ect. """
         self.response.out.write(*a, **kw)
 
     def render_str(self, template, **params):
-        """
-        Gets the template string and passes it with paramanters.
-        """
+        """ Gets the template string and passes it with paramanters. """
         tmp = JINJA_ENV.get_template(template)
         return tmp.render(params)
 
     def render(self, template, **kw):
         """
-        Calls render_str and displays the template
+        Calls render_str and displays the template.
         """
         self.write(self.render_str(template, **kw))
 
 
 class MainPage(Handler):
     """
-    Takes input from Handler and renders the markup defined.
+    Takes input from Handler and renders the markup
+    defined.
     """
     def get(self):
         """
@@ -45,8 +45,8 @@ class MainPage(Handler):
         # pases the value in item variable into the jinja2 template
         self.render("shopping_list.html", items=items)
 
-# Called in app.yaml as an atribute of this
-# file so that template can be rendered
+# Called in app.yaml as an atribute of thisfile so that template can be
+# rendered
 app = webapp2.WSGIApplication([
     ('/', MainPage)
 ], debug=True)
